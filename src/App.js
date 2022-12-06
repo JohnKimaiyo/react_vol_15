2f6206d6c2eb647e1ce5c2af8d33fe6e0e16153f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-06 21:46:04 +0300

    prop title called

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import Greet from './component/Greet';
-import Welcome from './component/Welcome';
+import React from "react";
+import "./App.css";
+import Greet from "./component/Greet";
+import Welcome from "./component/Welcome";
+import Hello from "./component/Hello";
+import Title from "./component/Title";
 function App() {
   return (
-  <div className='App'>
-    <Greet/>
-    <Welcome/>
-  </div>
-        
+    <div className="App">
+      <Greet />
+      <Welcome />
+      <Hello />
+      <Title name="Frontend Developer" />
+      <Title name="Solodity Developer" />
+      <Title name="Mobile Developer" />
+    </div>
   );
 }
 
